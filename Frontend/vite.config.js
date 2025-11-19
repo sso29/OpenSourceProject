841309c9c237ef02f2f23e08bc54d1a325b1b976
haim <gkgk3894@gmail.com>
2025-11-19 15:20:32 +0900

    1119

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,18 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',  // spring 서버
+        changeOrigin: true,
+        secure: false,
+      },
+      '/ai-api': { 
+        target: 'http://localhost:5001', // 5001 포트로 노출된 AI 서버
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/ai-api/, ''),
+    },
+    },
+  },
 })
