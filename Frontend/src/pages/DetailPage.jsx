25dcf081379a97c733317ddeb390cdb2f837e53a
haim <gkgk3894@gmail.com>
2025-11-26 15:48:14 +0900

    포트 변경 및 오류 수정 2

--- Frontend/src/pages/DetailPage.jsx
+++ Frontend/src/pages/DetailPage.jsx
@@ -134,7 +134,9 @@ const DetailPage = () => {
         
         // (주의) URL 인코딩: '선재 업고 튀어' -> '선재%20업고%20튀어'
         const encodedTitle = encodeURIComponent(id);
-        const response = await fetch(`http://localhost:5000/recommend/${encodedTitle}`);
+        // const response = await fetch(`http://localhost:5001/recommend/${encodedTitle}`);
+        const response = await fetch(`/ai-api/recommend/${encodedTitle}`);
+
 
         if (!response.ok) {
           const errData = await response.json();
